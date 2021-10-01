a743361bdf8d016d312395f0a99955a9fe11ec85
NahuelGallo <gallonahuel7@gmail.com>
2021-09-30 22:24:55 -0300

    DOM

--- js/main.js
+++ js/main.js
@@ -21,3 +21,18 @@ realizarCompra();
 
 
 alert('Muchas gracias por su compra!')
+
+let titulo = document.createElement("h1");
+
+titulo.textContent = "BIENVENIDOS A NUESTRA TIENDA ONLINE!";
+
+document.body.appendChild(titulo);
+
+mostrarProductos(datos); { 
+    const mercaderia = document.createElement("div")
+    mercaderia.classList.add("mercaderia");
+
+    const nombreMercaderia = document.createElement("h3");
+    nombreMercaderia.textContent = `${articulos.nombre}`
+    mercaderia.body.appendChild(mercaderia)
+}
